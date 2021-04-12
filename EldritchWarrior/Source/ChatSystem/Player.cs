ffedb80dfef24cbfae509a29ad5a187f7b048368
Scott Milliorn <scottmilliorn@gmail.com>
2021-04-11 23:22:49 -0700

    router

--- EldritchWarrior/Source/ChatSystem/Player.cs
+++ EldritchWarrior/Source/ChatSystem/Player.cs
@@ -15,17 +15,97 @@ namespace Source.ChatSystem
         {
             string message = NWScript.GetPCChatMessage();
             uint pc = NWScript.GetPCChatSpeaker();
-            
+
             if (TriggerChatTools(message))
             {
                 message = message[1..].ToLower();
-                Router(pc, message);
+                Router(pc, message.Split(' '));
             }
         }
 
-        private static void Router(uint pc, string message)
+        private static void Router(uint pc, string[] chatArray)
         {
-            throw new NotImplementedException();
+            switch (chatArray[0])
+            {
+                case "xp":
+                    _ = int.TryParse(chatArray[1], out int x);
+                    chat.Sender.Xp += x;
+                    break;
+                case "live":
+                    chat.Sender.HP = chat.Sender.HP = chat.Sender.MaxHP;
+                    break;
+                case "dead":
+                    chat.Sender.ApplyEffect(EffectDuration.Instant, NWN.API.Effect.Damage(1));
+                    break;
+                case "roster":
+                    Roster(chat);
+                    break;
+                case "armbone":
+                    SetArmBone(chat);
+                    break;
+                case "armskin":
+                    SetArmNormal(chat);
+                    break;
+                case "!":
+                    Emote(chat, chatArray);
+                    break;
+                case "head":
+                    SetHead(chat, chatArray);
+                    break;
+                case "portrait":
+                    SetPortrait(chat, chatArray);
+                    break;
+                case "voice":
+                    SetVoice(chat, chatArray);
+                    break;
+                case "skin":
+                    SetSkin(chat, chatArray);
+                    break;
+                case "hair":
+                    SetHair(chat, chatArray);
+                    break;
+                case "tattoocolor1":
+                    SetTattooColor1(chat, chatArray);
+                    break;
+                case "tattoocolor2":
+                    SetTattooColor2(chat, chatArray);
+                    break;
+                case "tail":
+                    SetTail(chat, chatArray);
+                    break;
+                case "wings":
+                    SetWings(chat, chatArray);
+                    break;
+                case "alignment":
+                    SetAlignment(chat, chatArray);
+                    break;
+                case "resetlevel":
+                    ResetLevel(chat, chatArray);
+                    break;
+                case "roll":
+                    RollDice(chat, chatArray);
+                    break;
+                case "status":
+                    SetStatus(chat, chatArray);
+                    break;
+                case "eyes":
+                    SetEyes(chat, chatArray);
+                    break;
+                case "visual":
+                    SetVisual(chat, chatArray);
+                    break;
+                case "lfg":
+                    NwModule.Instance.SpeakString($"{chat.Sender.Name.ColorString(Color.WHITE)} is looking for a party!", TalkVolume.Shout);
+                    break;
+                case "save":
+                    chat.Sender.ExportCharacter();
+                    break;
+                case "delete":
+                    chat.Sender.Delete($"{chat.Sender.BicFileName}.bic has been deleted.");
+                    break;
+                default:
+                    break;
+            }
         }
 
         private static bool TriggerChatTools(string message) => message.StartsWith(wildcard);
