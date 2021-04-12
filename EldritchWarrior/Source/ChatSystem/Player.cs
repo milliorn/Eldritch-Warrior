981368208c7cd227e8d62de7bf9b6109c414dbf1
Scott Milliorn <scottmilliorn@gmail.com>
2021-04-11 22:57:43 -0700

    ChatSystem

---
+++ EldritchWarrior/Source/ChatSystem/Player.cs
@@ -0,0 +1,12 @@
+using NWN.Framework.Lite;
+using NWN.Framework.Lite.Enum;
+using NWN.Framework.Lite.NWNX;
+using NWN.Framework.Lite.NWNX.Enum;
+
+namespace Source.ChatSystem
+{
+    public class Player
+    {
+        
+    }
+}
