e28479639024f8c939229f14a10c5bf0d55b49c6
Scott Milliorn <scottmilliorn@gmail.com>
2021-04-12 00:08:48 -0700

    unused using

--- EldritchWarrior/Source/ChatSystem/PlayerChat.cs
+++ EldritchWarrior/Source/ChatSystem/PlayerChat.cs
@@ -1,9 +1,7 @@
-using System;
 using System.Text;
 using NWN.Framework.Lite;
 using NWN.Framework.Lite.Enum;
 using NWN.Framework.Lite.NWNX;
-using NWN.Framework.Lite.NWNX.Enum;
 
 namespace Source.ChatSystem
 {
