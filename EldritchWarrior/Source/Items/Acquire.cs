bb252f80636766e44f58cb6116f131c3ef192f15
Scott Milliorn <scottmilliorn@gmail.com>
2021-04-14 16:48:16 -0700

    OnAcquireItem

---
+++ EldritchWarrior/Source/Items/Acquire.cs
@@ -0,0 +1,13 @@
+using NWN.Framework.Lite;
+
+namespace EldritchWarrior.Source.Items
+{
+    public class Acquire
+    {
+        [ScriptHandler("x2_mod_def_aqu")]
+        public static void OnAcquireItem()
+        {
+
+        }
+    }
+}
