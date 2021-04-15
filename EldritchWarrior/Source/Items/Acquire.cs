d19865b53e48a7e7b9bc6f80efed6df4ac8e938a
Scott Milliorn <scottmilliorn@gmail.com>
2021-04-14 19:00:54 -0700

    Item (#27)
    * OnAcquireItem
    
    * PrintGPValue
    
    * IPRemoveAllItemProperties
    
    * IPRemoveAllItemProperties
    
    * GetIsDM
    
    * BarterFix
    
    * SendMessageToAllPartyWithinDistance
    
    * stop spam
    
    * notify party
    
    * finished OnAcquireItem

---
+++ EldritchWarrior/Source/Items/Acquire.cs
@@ -0,0 +1,38 @@
+using System;
+using NWN.Framework.Lite;
+using NWN.Framework.Lite.Bioware;
+using NWN.Framework.Lite.Enum;
+
+namespace EldritchWarrior.Source.Items
+{
+    public class Acquire
+    {
+        [ScriptHandler("x2_mod_def_aqu")]
+        public static void OnAcquireItem()
+        {
+            uint acquired = NWScript.GetModuleItemAcquired();
+            uint by = NWScript.GetModuleItemAcquiredBy();
+            uint from = NWScript.GetModuleItemAcquiredFrom();
+
+            string name = NWScript.GetName(acquired);
+            string byName = NWScript.GetName(by);
+            string fromName = NWScript.GetName(from);
+
+            from.PrintGPValue();
+            BiowareXP2.IPRemoveAllItemProperties(acquired, DurationType.Temporary);
+
+            if (NWScript.GetIsDM(by)) return;
+
+            by.BarterFix(from);
+
+            if (String.IsNullOrEmpty(name))
+            {
+                by.SendMessageToAllPartyWithinDistance($"{byName} acquired Gold Pieces.", 40.0f);
+                return;
+            }
+
+            //Stop spam to combat log upon login
+            by.SendMessageToAllPartyWithinDistance($"{byName} acquired {name}.", 40.0f);
+        }
+    }
+}
---
+++ EldritchWarrior/Source/Items/Extensions.cs
@@ -0,0 +1,39 @@
+using System;
+using NWN.Framework.Lite;
+
+namespace EldritchWarrior.Source.Items
+{
+    public static class Extensions
+    {
+        public static string PrintGPValue(this uint item) => NWScript.GetPlotFlag(item)
+            ? $"Gold Piece Value:{NWScript.GetGoldPieceValue(item)}\n\n{NWScript.GetDescription(item, true, true)}"
+            : NWScript.GetDescription(item);
+
+        public static void BarterFix(this uint by, uint from)
+        {
+            if (NWScript.GetIsPC(by) && NWScript.GetIsPC(from))
+            {
+                NWScript.ExportSingleCharacter(by);
+                NWScript.ExportSingleCharacter(from);
+                NWScript.FloatingTextStringOnCreature("Character Saved.", by, false);
+                NWScript.FloatingTextStringOnCreature("Character Saved.", by, false);
+                //Execute Shifter code ws_saveall_sub
+            }
+        }
+
+        public static void SendMessageToAllPartyWithinDistance(this uint by, string message, float distance)
+        { 
+            uint factionMember = NWScript.GetFirstFactionMember(by);
+            uint area = NWScript.GetArea(by);
+
+            while(NWScript.GetIsObjectValid(factionMember))
+            {
+                if(NWScript.GetArea(factionMember) == area && NWScript.GetDistanceBetween(by, factionMember) <= distance)
+                {
+                    NWScript.FloatingTextStringOnCreature(message, factionMember);
+                }
+                factionMember = NWScript.GetNextFactionMember(by);
+            }
+        }
+    }
+}
