d887ba8eea309c3ed806def70798f042c75b8900
Scott Milliorn <scottmilliorn@gmail.com>
2021-04-14 20:09:21 -0700

    edit comment

---
+++ EldritchWarrior/Source/Client/Enter.cs
@@ -0,0 +1,13 @@
+using NWN.Framework.Lite;
+
+namespace EldritchWarrior.Source.Client
+{
+    public class Enter
+    {
+        [ScriptHandler("x3_mod_def_enter")]
+        public static void OnClientEnter()
+        {
+            
+        }
+    }
+}
