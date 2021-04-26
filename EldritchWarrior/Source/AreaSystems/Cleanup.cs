ba842874cc5b93c141149895d8f8a2e153f947c5
Scott Milliorn <scottmilliorn@gmail.com>
2021-04-25 20:15:53 -0700

    Area bugged

--- EldritchWarrior/Source/AreaSystems/Cleanup.cs
+++ EldritchWarrior/Source/AreaSystems/Cleanup.cs
@@ -9,14 +9,18 @@ namespace EldritchWarrior.Source.AreaSystems
         public static void Map()
         {
             uint objectInArea = NWScript.GetFirstObjectInArea();
-
             if (!objectInArea.PlayersRemainInArea())
             {
                 while (NWScript.GetIsObjectValid(objectInArea))
                 {
-                    switch(NWScript.GetObjectType(objectInArea))
+                    switch (NWScript.GetObjectType(objectInArea))
                     {
-                        case ObjectType.AreaOfEffect: objectInArea.DestroyAOE(); break;
+                        //case ObjectType.AreaOfEffect: objectInArea.DestroyAOE(); break;
+                        //case ObjectType.Creature: objectInArea.DestroyCreatureInArea(); break;
+                        //case ObjectType.Door: NWScript.AssignCommand(objectInArea, () => NWScript.ActionCloseDoor(objectInArea)); break;
+                        //case ObjectType.Item: objectInArea.DestroyItem(); break;
+                        //case ObjectType.Placeable: objectInArea.DestroyInventory(); break;
+                        default: break;
                     }
                 }
             }
--- EldritchWarrior/Source/AreaSystems/Extensions.cs
+++ EldritchWarrior/Source/AreaSystems/Extensions.cs
@@ -1,6 +1,4 @@
-using System;
 using NWN.Framework.Lite;
-using NWN.Framework.Lite.Enum;
 
 namespace EldritchWarrior.Source.AreaSystems
 {
@@ -27,84 +25,36 @@ namespace EldritchWarrior.Source.AreaSystems
             NWScript.DestroyObject(objectInArea);
         }
 
-        public static void DestroyCreaturesInArea(this uint objectInArea)
+        public static void DestroyCreatureInArea(this uint objectInArea)
         {
             uint area = NWScript.GetArea(objectInArea);
-            while (NWScript.GetIsObjectValid(objectInArea))
+            if (NWScript.GetHasInventory(objectInArea))
             {
-                if (Convert.ToBoolean(NWScript.GetObjectType(objectInArea) == ObjectType.Creature))
-                {
-                    if (NWScript.GetHasInventory(objectInArea))
-                    {
-                        objectInArea.DestroyCreatureInventory();
-                    }
-                    NWScript.AssignCommand(objectInArea, () => NWScript.SetIsDestroyable());
-                    NWScript.SetPlotFlag(objectInArea, false);
-                    NWScript.SetImmortal(objectInArea, false);
-                    NWScript.DestroyObject(objectInArea);
-                }
-                objectInArea = NWScript.GetNextObjectInArea();
+                objectInArea.DestroyInventory();
             }
+            NWScript.AssignCommand(objectInArea, () => NWScript.SetIsDestroyable());
+            NWScript.SetPlotFlag(objectInArea, false);
+            NWScript.SetImmortal(objectInArea, false);
+            NWScript.DestroyObject(objectInArea);
         }
 
-        public static void DestroyCreatureInventory(this uint objectInArea)
+        public static void DestroyInventory(this uint objectInArea)
         {
-            uint itemInInventory = NWScript.GetFirstItemInInventory(objectInArea);
+            uint itemInInventory = NWScript.GetFirstItemInInventory();
             while (NWScript.GetIsObjectValid(itemInInventory))
             {
                 NWScript.AssignCommand(itemInInventory, () => NWScript.SetIsDestroyable());
                 NWScript.SetPlotFlag(itemInInventory, false);
                 NWScript.DestroyObject(itemInInventory);
-                itemInInventory = NWScript.GetNextItemInInventory(objectInArea);
+                itemInInventory = NWScript.GetNextItemInInventory();
             }
         }
 
-        public static void ResetDoors(this uint objectInArea)
+        public static void DestroyItem(this uint objectInArea)
         {
-            uint area = NWScript.GetArea(objectInArea);
-            while (NWScript.GetIsObjectValid(objectInArea))
-            {
-                if (Convert.ToBoolean(NWScript.GetObjectType(objectInArea) == ObjectType.Door))
-                {
-                    NWScript.AssignCommand(objectInArea, () => NWScript.ActionCloseDoor(objectInArea));
-                }
-                objectInArea = NWScript.GetNextObjectInArea(area);
-            }
-        }
-
-        public static void DestroyItemsInArea(this uint objectInArea)
-        {
-            uint area = NWScript.GetArea(objectInArea);
-            while (NWScript.GetIsObjectValid(objectInArea))
-            {
-                if (NWScript.GetObjectType(objectInArea) == ObjectType.Item)
-                {
-                    NWScript.AssignCommand(objectInArea, () => NWScript.SetIsDestroyable());
-                    NWScript.SetPlotFlag(objectInArea, false);
-                    NWScript.DestroyObject(objectInArea);
-                }
-                objectInArea = NWScript.GetNextObjectInArea(area);
-            }
-        }
-
-        public static void ResetPlaceable(this uint objectInArea)
-        {
-            uint area = NWScript.GetArea(objectInArea);
-            while (NWScript.GetIsObjectValid(objectInArea))
-            {
-                if (Convert.ToBoolean(NWScript.GetObjectType(objectInArea) == ObjectType.Placeable))
-                {
-                    uint itemInInventory = NWScript.GetFirstItemInInventory(objectInArea);
-                    while (NWScript.GetIsObjectValid(itemInInventory))
-                    {
-                        NWScript.AssignCommand(itemInInventory, () => NWScript.SetIsDestroyable());
-                        NWScript.SetPlotFlag(itemInInventory, false);
-                        NWScript.DestroyObject(itemInInventory);
-                        itemInInventory = NWScript.GetNextItemInInventory(objectInArea);
-                    }
-                }
-                objectInArea = NWScript.GetNextObjectInArea(area);
-            }
+            NWScript.AssignCommand(objectInArea, () => NWScript.SetIsDestroyable());
+            NWScript.SetPlotFlag(objectInArea, false);
+            NWScript.DestroyObject(objectInArea);
         }
     }
 }
