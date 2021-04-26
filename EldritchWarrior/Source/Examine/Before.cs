1101cacd2f6185c177a0c8f55c9710d94ce745a9
Scott Milliorn <scottmilliorn@gmail.com>
2021-04-26 12:55:46 -0700

    fixed namespace

---
+++ EldritchWarrior/Source/Examine/Before.cs
@@ -0,0 +1,17 @@
+using NWN.Framework.Lite;
+using NWN.Framework.Lite.NWNX;
+
+namespace EldritchWarrior.Source.Examine
+{
+    public class Before
+    {
+        [ScriptHandler("on_exam_obj_befo")]
+        public static void OnBefore()
+        {
+            if (Events.GetCurrentEvent() == "NWNX_ON_EXAMINE_OBJECT_BEFORE")
+            {
+                System.Console.WriteLine("test");
+            }
+        }
+    }
+}
