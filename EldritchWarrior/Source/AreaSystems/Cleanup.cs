410df0e4804442380d614b0d7a57065a0fac45a1
Scott Milliorn <scottmilliorn@gmail.com>
2021-04-27 17:39:22 -0700

    are_exit_cleanup resolved

--- EldritchWarrior/Source/AreaSystems/Cleanup.cs
+++ EldritchWarrior/Source/AreaSystems/Cleanup.cs
@@ -1,6 +1,4 @@
-using System.Collections.Generic;
 using NWN.Framework.Lite;
-using NWN.Framework.Lite.Enum;
 
 namespace EldritchWarrior.Source.AreaSystems
 {
@@ -10,18 +8,17 @@ namespace EldritchWarrior.Source.AreaSystems
         public static void Map()
         {
             uint pc = NWScript.GetExitingObject();
-            uint area = NWScript.GetArea(NWScript.OBJECT_SELF);
+            uint area = NWScript.GetArea(pc);
             uint objectInArea = NWScript.GetFirstObjectInArea(area);
+/*
+            if (!Module.Extensions.GetIsClient(NWScript.GetExitingObject())) return;
 
-            if (!Module.Extensions.GetIsClient(pc)) return;
-
-            System.Console.WriteLine("before PlayersRemainInArea");
             if (area.PlayersRemainInArea()) return;
-            System.Console.WriteLine("after PlayersRemainInArea");
-
+*/
             while (NWScript.GetIsObjectValid(objectInArea))
             {
                 System.Console.WriteLine($"{NWScript.GetObjectType(objectInArea).ToString()}: {NWScript.GetName(objectInArea)}");
+                NWScript.DestroyObject(objectInArea);
                 objectInArea = NWScript.GetNextObjectInArea(area);
             }
         }
