07a792c595d7b3be00a528954f6bc5647cc15245
Scott Milliorn <scottmilliorn@gmail.com>
2021-05-06 21:19:39 -0700

    Cooldown (#38)
    * Cooldown.After.CombatFeat
    
    * Cooldown.Before.CombatFeat

--- EldritchWarrior/Source/Module/Load.cs
+++ EldritchWarrior/Source/Module/Load.cs
@@ -25,7 +25,11 @@ namespace EldritchWarrior.Source.Module
             InitAdministration();
             InitServerCalender();
             
-            NWN.Framework.Lite.NWNX.Events.SubscribeEvent("NWNX_ON_EXAMINE_OBJECT_BEFORE", "on_exam_obj_befo");
+            Events.SubscribeEvent("NWNX_ON_EXAMINE_OBJECT_BEFORE", "on_exam_obj_befo");
+
+            Events.SubscribeEvent("NWNX_ON_USE_FEAT_BEFORE", "on_use_feat_befo");
+            Events.SubscribeEvent("NWNX_ON_USE_FEAT_AFTER", "on_use_feat_afte");
+
             PrintBootTime();
         }
 
