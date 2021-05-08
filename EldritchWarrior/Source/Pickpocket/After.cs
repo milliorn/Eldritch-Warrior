c06fbe3b2749c7052c8241a0db5459e651ed1b63
Scott Milliorn <scottmilliorn@gmail.com>
2021-05-07 21:16:55 -0700

    Pickpocket.After.OnSkillUsed

---
+++ EldritchWarrior/Source/Pickpocket/After.cs
@@ -0,0 +1,13 @@
+using NWN.Framework.Lite;
+
+namespace EldritchWarrior.Source.Pickpocket
+{
+    public class After
+    {
+        [ScriptHandler("on_use_skill_aft")]
+        public static void OnSkillUsed()
+        {
+
+        }
+    }
+}
