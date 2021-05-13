320ba3d7e33e1c55f937bbdbdc776af18fa882ee
Scott Milliorn <scottmilliorn@gmail.com>
2021-05-12 18:42:02 -0700

    CreateBankObject

--- EldritchWarrior/Source/Bank/Extensions.cs
+++ EldritchWarrior/Source/Bank/Extensions.cs
@@ -1,5 +1,6 @@
 using NWN.Framework.Lite;
 using static NWN.Framework.Lite.NWScript;
+using NWN.Framework.Lite.Enum;
 
 namespace EldritchWarrior.Source.Bank
 {
@@ -7,6 +8,13 @@ namespace EldritchWarrior.Source.Bank
     {
         public static readonly int maxItems = 30;
 
-        public static string dbLocationCDKEY(string cdkey) => $"{GetModuleName()}:{cdKey}";
+        public static string DBLocationCDKEY(this string cdkey) => $"{GetModuleName()}:{cdkey}";
+
+        public static uint CreateBankObject(uint pc, string chestTag)
+        {
+            uint bank = CreateObject(ObjectType.Placeable, "_bank_", GetLocation(pc));
+            SetLocalString(bank, "CHEST_TAG", chestTag);
+            return bank;
+        }
     }
 }
