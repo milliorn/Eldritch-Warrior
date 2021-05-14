2fe1fdad186190ba0f751a8ad2f51f770c6dff74
Scott Milliorn <scottmilliorn@gmail.com>
2021-05-13 20:55:04 -0700

    removed unused

--- EldritchWarrior/Source/ItemBank/Extensions.cs
+++ EldritchWarrior/Source/ItemBank/Extensions.cs
@@ -1,60 +1,8 @@
-using static NWN.Framework.Lite.NWScript;
-using NWN.Framework.Lite.Enum;
-
 namespace EldritchWarrior.Source.ItemBank
 {
     public static class Extensions
     {
         public static readonly int maxItems = 30;
         public static readonly string itemBankName = "BANK_ITEM";
-        public static string DBLocationCDKEY(this string cdkey) => $"{GetModuleName()}:{cdkey}";
-
-        public static bool ItemDepositSuccessful(this uint chest, uint pc)
-        {
-            //iterate over all items in the inventory
-            int count = 0;
-            uint item = GetFirstItemInInventory(chest);
-
-            while (GetIsObjectValid(item))
-            {
-                //Item count
-                count++;
-
-                if (GetHasInventory(item))
-                {
-                    RefuseItem(pc, item);
-                    FloatingTextStringOnCreature("No Bags allowed!", pc, false);
-                    return false;
-                }
-
-                //repeated above
-                else if (count > maxItems)
-                {
-                    pc.RefuseItem(item);
-                    FloatingTextStringOnCreature("Maximum Item Count Exceeded!", pc, false);
-                    return false;
-                }
-
-                //Next item
-                item = GetNextItemInInventory(chest);
-            }
-
-            return true;
-        }
-
-        public static void RefuseItem(this uint pc, uint item)
-        {
-            DestroyObject(item, 0.2f);
-            CopyItem(NWN.Framework.Lite.NWNX.Object.Deserialize(NWN.Framework.Lite.NWNX.Object.Serialize(item)));
-        }
-
-        public static uint CreateBankObject(uint pc, string chestTag)
-        {
-            uint bank = CreateObject(ObjectType.Placeable, "_bank_", GetLocation(pc));
-            SetLocalString(bank, "CHEST_TAG", chestTag);
-            return bank;
-        }
-
-        public static uint GetBankObject(this uint pc, string chestTag) => CreateBankObject(pc, chestTag);
     }
 }
