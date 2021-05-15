bf39bf933e40bcd6e2d768607e0372284a105fe4
Scott Milliorn <scottmilliorn@gmail.com>
2021-05-14 19:13:20 -0700

    updated scriptname

--- EldritchWarrior/Source/AreaSystems/Cleanup.cs
+++ EldritchWarrior/Source/AreaSystems/Cleanup.cs
@@ -5,32 +5,32 @@ using static NWN.Framework.Lite.NWScript;
 
 namespace EldritchWarrior.Source.AreaSystems
 {
-	public class Cleanup
-	{
-		[ScriptHandler("are_exit_cleanup")]
-		public static void Map()
-		{
-			uint pc = GetExitingObject();
-			uint area = GetArea(pc);
-			uint objectInArea = GetFirstObjectInArea(area);
+    public class Cleanup
+    {
+        [ScriptHandler("area_exit")]
+        public static void Map()
+        {
+            uint pc = GetExitingObject();
 
             PlayerCharacter.Extensions.Save(pc);
-			
-            if (!area.PlayersRemainInArea())
-			{
-				while (GetIsObjectValid(objectInArea))
-				{
-					switch (GetObjectType(objectInArea))
-					{
-						case ObjectType.AreaOfEffect:
-						case ObjectType.Creature:
-						case ObjectType.Item:DestroyObject(objectInArea); break;
-						case ObjectType.Door: PlayAnimation(AnimationType.DoorClose); break;
-						case ObjectType.Placeable: objectInArea.DestroyInventory(); break;
-					}
-					objectInArea = GetNextObjectInArea(area);
-				}
-			}
-		}
-	}
+            System.Console.WriteLine(GetName(pc));
+
+            uint area = GetArea(pc);
+            if (area.PlayersRemainInArea()) return;
+
+            uint objectInArea = GetFirstObjectInArea(area);
+            while (GetIsObjectValid(objectInArea))
+            {
+                switch (GetObjectType(objectInArea))
+                {
+                    case ObjectType.AreaOfEffect:
+                    case ObjectType.Creature:
+                    case ObjectType.Item: DestroyObject(objectInArea); break;
+                    case ObjectType.Door: PlayAnimation(AnimationType.DoorClose); break;
+                    case ObjectType.Placeable: objectInArea.DestroyInventory(); break;
+                }
+                objectInArea = GetNextObjectInArea(area);
+            }
+        }
+    }
 }
