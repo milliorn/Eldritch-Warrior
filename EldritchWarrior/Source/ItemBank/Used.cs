33b5ad106100a63a6c1d261fefd27bc74ccf7839
Scott Milliorn <scottmilliorn@gmail.com>
2021-05-15 20:20:43 -0700

    ItemBank.Used

--- EldritchWarrior/Source/ItemBank/Used.cs
+++ EldritchWarrior/Source/ItemBank/Used.cs
@@ -10,15 +10,14 @@ namespace EldritchWarrior.Source.ItemBank
         {
             // Vars
             uint pc = GetLastUsedBy();
-            uint chest = OBJECT_SELF;
-            string id = GetPCPublicCDKey(pc);
-            string userID = GetLocalString(chest, "USER_ID");
 
             // End script if any of these conditions are met
             if (!GetIsPC(pc) || GetIsDM(pc) || GetIsDMPossessed(pc) || GetIsPossessedFamiliar(pc)) return;
 
+            uint chest = OBJECT_SELF;
+            string userID = GetLocalString(chest, "USER_ID");
             // If the chest is already in use then this must be a thief
-            if (userID != "" && userID != id)
+            if (userID != "" && userID != GetPCPublicCDKey(pc))
             {
                 AssignCommand(pc, () => ActionMoveAwayFromObject(chest));
                 return;
